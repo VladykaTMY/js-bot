d00f9b5de8ee382f74f9cf4dee7d18e1cecffef2
VladykaTMY <82215415+VladykaTMY@users.noreply.github.com>
2021-04-26 13:31:45 +0300

    Create google_bot.js

---
+++ google_bot/google_bot.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Bot for Google
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.google.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+let keywords = ["Вывод произвольных полей wordpress", "10  самых популярных шрифтов от Google","Отключение редакций и ревизий в WordPress"]
+document.getElementsByName("q")[0].value = "10 самых популярных шрифтов от Google";
+let btnK = document.getElementsByName("btnK")[0];
+let links = document.links;
+let keyword = keywords[getRandom(0,keywords.length)];
+// если бтнк не равнен андефайнд тогда мы кликаем по кнопке. Иначе перебираем ссылки
+if(btnK !== undefined){
+    document.getElementsByName("btnK")[0].click();
+}else{
+    for(let i =0; i<links.length; i++){
+        if (links[i].href.indexOf('napli.ru')!=-1){
+            let link = links[i];
+            console.log("Нашел фразу " + link);
+            link.click();
+            break;
+        }
+    }
+}
+
+function getRandom(min, max) {
+    return Math.floor(math.random()*(max-min)+min);
+}
