79304e0ebd39728a47dfedcb24a1821656bc7a22
VladykaTMY <82215415+VladykaTMY@users.noreply.github.com>
2021-04-27 19:12:37 +0300

    Обновили возможности:
    а) Добавлены возможности вызова функций через определенное время
    !!! ВАЖНО: от балды интервалы не ставить - функции не успевают действовать.
    б) Возможность печетать по одной букве ключевую фразу в строку поиска
    в) Бот может прогуливаться по сайту и с вероятностью 10% вернуться обратно в поисковик
    г)возможность путешествия по глубинам поиска на n число страниц вперед и вернуться в поисковик

--- Yandex_bot/yandex_bot.js
+++ Yandex_bot/yandex_bot.js
@@ -5,26 +5,69 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://yandex.ru/*
+// @match        https://xn----8sbabr6ahc3e.xn--p1ai/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
-let keywords = ["гобой","кларнет","как звучит флейта"];
-let keyword = keywords[getRandom(0, keywords.length)];
-let links = document.links;
+let keywords = ["тюнинг приоры ", "Подвеска двигателя нового образца"," руководство лада калина"];
 let btnN = document.getElementsByClassName("button_theme_search")[0]
+let links = document.links;
+let keyword = keywords[getRandom(0, keywords.length)];
+let yaInput = document.getElementById("text");
+let i = 0;
+
 if(btnN!== undefined){
-    document.getElementById("text").value = keyword;
-    btnN.click()
-}else{
-    for(let i=0;i<links.length;i++){
-        if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")!=-1){
+    let timerId = setInterval(()=>{
+        yaInput.value += keyword[i];
+        i++;
+        if(i==keyword.length){
+            clearInterval(timerId);
+            btnN.click();
+        }
+    },250);
+
+}else if(location.hostname == "xn----8sbabr6ahc3e.xn--p1ai" ){//на сайте лада.онлайн кликаем рандомные ссылки и с вероятностью 70% попадаем на яндекс
+    console.log("работает");
+    setTimeout(()=>{
+        let index = getRandom(0,links.length);
+        if(getRandom(0,101)>=10){
+            location.href = "https://yandex.ru";
+        }
+        if(links[index].href.indexOf("xn----8sbabr6ahc3e.xn--p1ai")!=-1)
+            links[index].click();
+    }, getRandom(1000,2000));
+
+}
+
+
+else{
+    let nextYaPage = true //Добавляем переменную переключающую на следующую страницу
+    for(let i=0; i<links.length; i++){
+        if(links[i].href.indexOf("xn----8sbabr6ahc3e.xn--p1ai")!=-1){
             let link = links[i];
             console.log("Найдено "+link);
-            link.removeAttribute("target")
-            link.click();
+            nextYaPage = false;
+            setTimeout(()=>{// задержка клика
+                link.removeAttribute("target");
+                link.click();}
+                       ,getRandom(100,500));
             break;
         }
+    }
+    if(document.querySelector('.pager__item_current_yes').ariaLabel =="Текущая страница 4"){
+        nextYaPage = false;
+        setTimeout(()=>{
+
+
+            location.href = "https://yandex.ru";}
+                   ,getRandom(3000,4000));
+
+    }
+    if (document.querySelector(".pager__item_current_yes").ariaLabel !=="Текущая страница 4"){// нажимаем на кнопку следующая страница в течении 3-5 сек
+        setTimeout(()=>{
+            document.querySelector(".pager__item_kind_next").click();}
+                   ,getRandom(3000,4000));
     }
 }
 
