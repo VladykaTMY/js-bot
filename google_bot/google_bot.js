ef8fbf321b8b09eab394d3e846a5fb6dd93ef6e4
VladykaTMY <82215415+VladykaTMY@users.noreply.github.com>
2021-04-28 19:31:13 +0300

    Добавлено
    1.Сайты и ключевые слова объединены в одну переменную
    2.Возможность случайного выбора сайта
    3.Добавлена функция getCookie
    4.Изменены интервалы

--- google_bot/google_bot.js
+++ google_bot/google_bot.js
@@ -6,19 +6,43 @@
 // @author       You
 // @match        https://www.google.com/*
 // @match        https://napli.ru/*
+// @match        https://psyholog.me/*
+// @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
-let keywords = ["Вывод произвольных полей wordpress", "10  самых популярных шрифтов от Google","Отключение редакций и ревизий в WordPress"]
 
+
+let sites = {
+    "napli.ru":["10 самых популярных шрифтов от Google ",
+                "Отключение редакций и ревизий в WordPress ",
+                "Вывод произвольных типов записей и полей в WordPress"],
+    "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai":["гобой",
+                                           "Как звучит флейта",
+                                           "Кларнет", "Валторна"],
+    "psyholog.me":["центр здоровых отношений ",
+                   "Услуги центра здоровых отношений ",
+                   "Чекалина Елена психолог "]
+}
+
+let site = Object.keys(sites)[getRandom(0,Object.keys(sites).length)];
+let keywords = sites[site]
 let btnK = document.getElementsByName("btnK")[0];
 let links = document.links;
 let keyword = keywords[getRandom(0,keywords.length)];
 let googleInput = document.getElementsByName("q")[0];
 let i=0;
 
+if(btnK !== undefined){//если бнтн кей не равно андефайнд, то мы в куки кладем из переменной сайт, иначе когда мы на гугле мы кладем куки из сайт. и если мы не на гугле то мы кладем из переменной сайт
+    document.cookie = "site=" + site;
+}else if (location.hostname == "www.google.com"){
+    site = getCookie("site");
+}else {
+    site = location.hostname;
+}
 
 if(btnK !== undefined){
+    document.cookie = "site="+site;
     let timerId = setInterval (()=>{
         googleInput.value += keyword[i];
         i++;
@@ -26,42 +50,45 @@ if(btnK !== undefined){
             clearInterval(timerId)
             btnK.click();
         }
-    },1000);
+    },100)
 
+    }else if(location.hostname == site){
+        console.log("это работает");
+        setTimeout(()=>{
+            let index = getRandom(0,links.length);
+            if(getRandom(0,101)>=50){
+                location.href = "https://www.google.com/";
+            }
+            if(links[index].href.indexOf(site)!=-1)
+                links[index].click();
+        },getRandom(4000,7000));
 
-}else if(location.hostname == "napli.ru"){
-    console.log("это работает");
-    setTimeout(()=>{
-        let index = getRandom(0,links.length);
-        if(getRandom(0,101)>=70){
-            location.href = "https://www.google.com/";
-        }
-        if(links[index].href.indexOf('napli.ru')!=-1)
-            links[index].click();
-    },getRandom(2000,3500));
-
-}
+    }
 else{
     let nextGooglePage = true;
     for(let i =0; i<links.length; i++){
-        if (links[i].href.indexOf('napli.ru')!=-1){
+        if (links[i].href.indexOf(site)!=-1){
             let link = links[i];
             console.log("Нашел фразу " + link);
             nextGooglePage = false;
             setTimeout(()=>{
                 link.click();}
-                       ,getRandom(1000,4500));
+                       ,getRandom(3000,5000));
             break;
         }
     }
     if(document.querySelector('.YyVfkd').innerText == "5"){
         nextGooglePage = false;
-        setTimeout(()=>{
-            location.href = "https://www.google.com/";}
-                   ,getRandom(3000,5000));
+        location.href = "https://www.google.com/";
     }
 
-    if(document.querySelector('.YyVfkd').innerText !== "5"){
+
+    //    setTimeout(()=>{
+    //        location.href = "https://www.google.com/";}
+    //               ,getRandom(3000,5000));
+    //}
+
+    if(nextGooglePage){
         setTimeout(()=>{
             pnnext.click();}
                    ,getRandom(3000,5000));
@@ -69,4 +96,11 @@ else{
 }
 function getRandom(min, max) {
     return Math.floor(Math.random()*(max-min)+min);
+}
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
 }
